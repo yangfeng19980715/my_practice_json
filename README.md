016e921a58561ceaf5a33d15b7ade28c6178e20c
yangfeng-pc-company <yangfeng19980715@gmail.com>
2022-09-08 11:17:02 +0800

    first commit of json

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # my_practice_json
+
+This is my practice of json parser.
+I learn it from leptjson.
---
+++ header/headers.h
@@ -0,0 +1,31 @@
+//
+// Created by yangfeng on 2022/9/8.
+//
+
+#ifndef MY_PRACTICE_JSON_HEADERS_H
+#define MY_PRACTICE_JSON_HEADERS_H
+
+#include <iostream>
+#include <vector>
+#include <ranges>
+#include <random>
+#include <iomanip>
+#include <map>
+#include <memory>
+#include <type_traits>
+#include <list>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <set>
+#include <deque>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <numeric>
+#include <algorithm>
+#include <utility>
+#include <stack>
+#include <functional>
+
+#endif //MY_PRACTICE_JSON_HEADERS_H
